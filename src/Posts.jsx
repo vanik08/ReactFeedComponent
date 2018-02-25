61f79de1a258857736d71188466f9b3d8503ffca
Vanik Gevorgyan <vanik08@gmail.com>
2018-02-25 00:33:00 -0800

    Init Post component to render post information

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
+import PropTypes from 'prop-types';
+import Post from './Post';
 
 import withFeed from './withFeed';
 
 class Posts extends Component {
+  static propTypes = {
+    posts: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number,
+        created_at: PropTypes.string,
+        user: PropTypes.shape({
+          name: PropTypes.string,
+        }),
+        text: PropTypes.string,
+      }),
+    ).isRequired,
+  };
+
   render() {
     return (
       <div className="Posts">
-        <RaisedButton label="Test" onClick={console.log} />
+        {this.props.posts.map(post => (
+          <Post
+            key={post.id}
+            postDate={post.created_at}
+            authorName={post.user.name}
+            messageBody={post.text}
+          />
+        ))}
       </div>
     );
   }
